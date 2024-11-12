d3f835975be9a35a5eee1e545713b5ad3af38695
maxmwangikenya <maxmwangikenya@gmail.com>
2024-11-12 17:40:11 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -102,4 +102,61 @@ $(document).ready(function () {
     responsive: { 0: { items: 1 }, 576: { items: 2 }, 768: { items: 3 }, 992: { items: 4 } }
   });
 });
+let navbarToggler = document.querySelector(".navbar-toggler");
+const navbarCollapse = document.querySelector(".navbar-collapse");
 
+document.querySelectorAll(".ud-menu-scroll").forEach((e) =>
+  e.addEventListener("click", () => {
+    navbarToggler.classList.remove("active");
+    navbarCollapse.classList.remove("show");
+  })
+);
+navbarToggler.addEventListener("click", function () {
+  navbarToggler.classList.toggle("active");
+  navbarCollapse.classList.toggle("show");
+});
+
+// ===== submenu
+const submenuButton = document.querySelectorAll(".nav-item-has-children");
+submenuButton.forEach((elem) => {
+  elem.querySelector("a").addEventListener("click", () => {
+    elem.querySelector(".ud-submenu").classList.toggle("show");
+  });
+});
+
+// ===== wow js
+new WOW().init();
+
+// ====== scroll top js
+function scrollTo(element, to = 0, duration = 500) {
+  const start = element.scrollTop;
+  const change = to - start;
+  const increment = 20;
+  let currentTime = 0;
+
+  const animateScroll = () => {
+    currentTime += increment;
+
+    const val = Math.easeInOutQuad(currentTime, start, change, duration);
+
+    element.scrollTop = val;
+
+    if (currentTime < duration) {
+      setTimeout(animateScroll, increment);
+    }
+  };
+
+  animateScroll();
+}
+
+Math.easeInOutQuad = function (t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t + b;
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+};
+
+// Scroll to top functionality
+document.querySelector(".back-to-top").onclick = () => {
+  scrollTo(document.documentElement);
+};
